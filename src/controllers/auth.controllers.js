485e2611e2ba94bd0fc871920cc0103067f04933
digimon-codax <chatterjeeanish49@gmail.com>
2026-05-04 22:52:42 +0530

    add login functionality with JWT authentication and cookie handling

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -27,4 +27,30 @@ async function register(req, res){
   })
 }
 
-  module.exports = {register}
+async function login(req, res){
+  const {email, password} = req.body
+  const user = await userModel.findOne({email}).select('+password')
+
+  if(!user){
+    return res.status(401).json({message: "Invalid email or password"})
+  }
+
+  const isPasswordValid = await user.comparePassword(password)
+
+  if(!isPasswordValid){
+    return res.status(401).json({message: "Invalid email or password"})
+  }
+
+  const token = jwt.sign({id: user._id}, process.env.JWT_SECRET)
+  res.cookie('token', token)
+  return res.status(200).json({message: "User logged in successfully",
+    user:{
+      id: user._id,
+      name: user.name,
+      email: user.email
+
+    },token
+  })
+}
+
+  module.exports = {register, login}
