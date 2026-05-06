4464f5baad1ba6b0ed10181ea5817c69ba1094f2
digimon-codax <chatterjeeanish49@gmail.com>
2026-05-06 23:49:43 +0530

    add transaction model with fromAccount, toAccount, status, amount, and idempotencyKey fields

---
+++ src/models/transaction.models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const transactionSchema = new mongoose.Schema({
+  fromAccount: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Account',
+    required: [true, 'From account is required'],
+    index: true
+  },
+  toAccount: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Account',
+    required: [true, 'To account is required'],
+    index: true
+  },
+  status: {
+    type: String,
+    enum: {
+      values: ['PENDING', 'COMPLETED', 'FAILED', 'REVERSED'],
+      message: 'Status should be either PENDING, COMPLETED, FAILED or REVERSED',
+    },
+    default: 'PENDING'
+  },
+  amount: {
+    type: Number,
+    required: [true, 'Amount is required'],
+    min: [0, 'Amount must be positive']
+  },
+  idempotencyKey: {
+    type: String,
+    required: [true, 'Idempotency key is required'],
+    unique: true,
+    index: true
+  }
+}, {timestamps: true})
