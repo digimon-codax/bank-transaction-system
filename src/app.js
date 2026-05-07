c495a054a26d497925570b2daa979b253ff6fb90
digimon-codax <chatterjeeanish49@gmail.com>
2026-05-07 22:50:26 +0530

    add health check endpoint to confirm server is running

--- src/app.js
+++ src/app.js
@@ -6,10 +6,16 @@ const transactionRoutes = require('./routes/transaction.routes')
 
 const app = express()
 
+
+
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/accounts', accountRoutes)
 app.use('/api/transactions', transactionRoutes)
+
+app.get('/', (req, res) => {
+  res.send('ledger system is running')
+})
 
 module.exports = app
